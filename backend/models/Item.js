f120d086eaaf94cba51d3b189e03bf5245b0cb2f
Stanley  Pierre-Louis <stanleypl75@gmail.com>
2023-06-06 20:46:15 +0000

    add isVerified

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -12,7 +12,11 @@ var ItemSchema = new mongoose.Schema(
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
-    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    isVerified: {
+      type: Boolean,
+      default:false
+    }
   },
   { timestamps: true }
 );
@@ -55,7 +59,8 @@ ItemSchema.methods.toJSONFor = function(user) {
     tagList: this.tagList,
     favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
-    seller: this.seller.toProfileJSONFor(user)
+    seller: this.seller.toProfileJSONFor(user),
+    isVerified:this.isVerified
   };
 };
 
